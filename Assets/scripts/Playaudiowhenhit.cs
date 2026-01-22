a1c7e155721b0db6de41cbafc451ba6d131d4a2f
Sem Doeswijk <40010@ma-web.nl>
2026-01-22 12:55:38 +0100

    Initial commit

---
+++ Assets/scripts/Playaudiowhenhit.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.Play();
+        }
+    }
+}
